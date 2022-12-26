9f536eda24b24f584260894e4687f042bf87448d
Jac0bhung <vasortgrav@gmail.com>
2022-12-26 22:23:01 +0800

    依據助教11 12/23意見修改

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,11 +9,17 @@ router.get("/", (req, res) => {
   restaurant
     .find({})
     .lean()
+    .sort({ _id: "desc" })
     .then((restaurantsData) => res.render("index", { restaurantsData }))
-    .catch((err) => console.log(err));
+    // .catch((err) => console.log(err));
+    //助教A11建議
+    .catch((err) => {
+      console.log(err);
+      res.render("errorPage", { error: err.message });
+    });
 });
 
-//搜尋餐廳 
+//搜尋餐廳
 router.get("/search", (req, res) => {
   if (!req.query.keywords) {
     res.redirect("/");
@@ -33,7 +39,41 @@ router.get("/search", (req, res) => {
       );
       res.render("index", { restaurantsData: filterRestaurantsData, keywords });
     })
-    .catch((err) => console.log(err));
+    // .catch((err) => console.log(err));
+    //助教A11建議
+    .catch((err) => {
+      console.log(err);
+      res.render("errorPage", { error: err.message });
+    });
+});
+
+// 參考
+router.get("/sort", (req, res) => {
+  const sortMethod = req.query.sortMethod;
+  let sortBy;
+  if (sortMethod === "A -> Z") {
+    sortBy = { name_en: "asc" };
+  }
+  if (sortMethod === "Z -> A") {
+    sortBy = { name_en: "desc" };
+  }
+  if (sortMethod === "推薦指數:高至低") {
+    sortBy = { rating: "desc" };
+  }
+  if (sortMethod === "推薦指數:低至高") {
+    sortBy = { rating: "asc" };
+  }
+  restaurant
+    .find({})
+    .lean()
+    .sort(sortBy)
+    .then((restaurantsData) =>
+      res.render("index", { restaurantsData, sortMethod })
+    )
+    .catch((error) => {
+      console.log(error);
+      res.render("error", { error_message: error.message });
+    });
 });
 
 module.exports = router;
--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -12,7 +12,12 @@ router.post("/", (req, res) => {
   restaurant
     .create(req.body)
     .then(() => res.redirect("/"))
-    .catch((err) => console.log(err));
+    // .catch((err) => console.log(err));
+    //助教A11建議
+    .catch((err) => {
+      console.log(err);
+      res.render("errorPage", { error: err.message });
+    });
 });
 
 //瀏覽特定餐廳
@@ -22,7 +27,12 @@ router.get("/:id", (req, res) => {
     .findById(id)
     .lean()
     .then((restaurantData) => res.render("show", { restaurantData }))
-    .catch((err) => console.log(err));
+    // .catch((err) => console.log(err));
+    //助教A11建議
+    .catch((err) => {
+      console.log(err);
+      res.render("errorPage", { error: err.message });
+    });
 });
 
 //讀取編輯餐廳頁面
@@ -32,7 +42,12 @@ router.get("/:id/edit", (req, res) => {
     .findById(id)
     .lean()
     .then((restaurantData) => res.render("edit", { restaurantData }))
-    .catch((err) => console.log(err));
+    // .catch((err) => console.log(err));
+    //助教A11建議
+    .catch((err) => {
+      console.log(err);
+      res.render("errorPage", { error: err.message });
+    });
 });
 
 // 修改餐廳資料資料;
@@ -53,17 +68,29 @@ router.put("/:id", (req, res) => {
       return restaurantData.save();
     })
     .then(() => res.redirect(`/restaurants/${id}`))
-    .catch((err) => console.log(err));
+    // .catch((err) => console.log(err));
+    //助教A11建議
+    .catch((err) => {
+      console.log(err);
+      res.render("errorPage", { error: err.message });
+    });
 });
 
 //刪除餐廳
 router.delete("/:id", (req, res) => {
   const id = req.params.id;
-  return restaurant
-    .findById(id)
-    .then((restaurant) => restaurant.remove())
-    .then(() => res.redirect("/"))
-    .catch((error) => console.log(error));
+  return (
+    restaurant
+      .findById(id)
+      .then((restaurant) => restaurant.remove())
+      .then(() => res.redirect("/"))
+      // .catch((error) => console.log(error));
+      //助教A11建議
+      .catch((err) => {
+        console.log(err);
+        res.render("errorPage", { error: err.message });
+      })
+  );
 });
 
 module.exports = router;
